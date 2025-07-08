1b660031ade9c6d226c2e2818708c198ceeacbbb
Alona Kuznietsova <acvetochka@gmail.com>
2025-07-08 22:32:14 +0200

    add type to strapi-configs

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,16 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+type EnvFn = {
+  (key: string, defaultValue?: string): string;
+  int(key: string, defaultValue?: number): number;
+  bool(key: string, defaultValue?: boolean): boolean;
+  array(key: string, defaultValue?: string[], separator?: string): string[];
+  json<T = unknown>(key: string, defaultValue?: T): T;
+};
+
+export default ({ env }: { env: EnvFn }) => ({
+  // export default ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
